898c8ca178ffb5670e498a5e2aab4b5333a6660e
Yahia LAMRI <yahia.lamri@outlook.fr>
2020-05-01 15:28:07 +0200

    add testing

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,5 +1,25 @@
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
+const request = require('supertest');
+const app = require('../app');
+const MongoClient = require('mongodb');
+
+
+test('db connect', async () => {
+    const connection = await MongoClient.connect('mongodb://localhost/shoppinglist', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    let db = await connection.db();
+    expect(db.databaseName).toBe("shoppinglist")
+});
+
+test('should test that true === true', () => {
     expect(true).toBe(true);
-  });
+});
+
+test('insert user', async () => {
+    const res = await request(app).post('/register').send({
+        username: 'testing',
+        password: 'pass',
+    })
+    expect(res.statusCode).toBe(200);
 });
