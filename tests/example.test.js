953e1d812a5bf18d1886587fe31888e327429263
Yahia LAMRI <yahia.lamri@outlook.fr>
2020-05-01 20:03:40 +0200

    new : can delete user

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,25 +1,25 @@
 const request = require('supertest');
-const app = require('../app');
 const MongoClient = require('mongodb');
+const app = require('../app');
 
 
 test('db connect', async () => {
-    const connection = await MongoClient.connect('mongodb://localhost/shoppinglist', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    let db = await connection.db();
-    expect(db.databaseName).toBe("shoppinglist")
+  const connection = await MongoClient.connect('mongodb://localhost/shoppinglist', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = await connection.db();
+  expect(db.databaseName).toBe('shoppinglist');
 });
 
 test('should test that true === true', () => {
-    expect(true).toBe(true);
+  expect(true).toBe(true);
 });
 
 test('insert user', async () => {
-    const res = await request(app).post('/register').send({
-        username: 'testing',
-        password: 'pass',
-    })
-    expect(res.statusCode).toBe(200);
+  const res = await request(app).post('/register').send({
+    username: 'testing',
+    password: 'pass',
+  });
+  expect(res.statusCode).toBe(200);
 });
